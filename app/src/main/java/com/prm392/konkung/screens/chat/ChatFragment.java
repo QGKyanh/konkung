17964cf1205a2c1f459d9ab878ec40c19c7e1937
nguyenanhquan04 <fpskirito2004@gmail.com>
2025-06-04 21:31:43 +0700

    Initial Commit

---
+++ app/src/main/java/com/prm392/konkung/screens/chat/ChatFragment.java
@@ -0,0 +1,32 @@
+package com.prm392.konkung.screens.chat;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.prm392.konkung.R;
+
+public class ChatFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        
+        setupViews(view);
+    }
+
+    private void setupViews(View view) {
+        // TODO: Setup chat screen views and functionality
+    }
+}
---
+++ app/src/main/java/com/prm392/konkung/screens/main/MainActivity.java
@@ -0,0 +1,72 @@
+package com.prm392.konkung.screens.main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.prm392.konkung.R;
+import com.prm392.konkung.screens.chat.ChatFragment;
+import com.prm392.konkung.screens.home.HomeFragment;
+import com.prm392.konkung.screens.map.MapFragment;
+import com.prm392.konkung.screens.profile.ProfileFragment;
+import com.prm392.konkung.screens.shop.ShopFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        setupBottomNavigation();
+        
+        // Load default fragment
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment());
+        }
+    }
+
+    private void initViews() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+    }
+
+    private void setupBottomNavigation() {
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (itemId == R.id.nav_shop) {
+                selectedFragment = new ShopFragment();
+            } else if (itemId == R.id.nav_chat) {
+                selectedFragment = new ChatFragment();
+            } else if (itemId == R.id.nav_map) {
+                selectedFragment = new MapFragment();
+            } else if (itemId == R.id.nav_profile) {
+                selectedFragment = new ProfileFragment();
+            }
+
+            if (selectedFragment != null) {
+                return loadFragment(selectedFragment);
+            }
+            return false;
+        });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
